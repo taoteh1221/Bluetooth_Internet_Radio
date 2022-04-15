f7920cd33672eb5f4e527e0b5aea91b920236f6b
taoteh1221 <mike@dragonfrugal.com>
2022-04-15 16:20:25 -0400

    docs / example csv revisions
    docs / example csv revisions

--- bt-radio-setup.bash
+++ bt-radio-setup.bash
@@ -10,7 +10,7 @@
 # Fully automated setup of bluetooth and an internet radio player (PyRadio), on a headless RaspberryPi,
 # connecting to a stereo system's bluetooth receiver (bash script, chmod +x it to run).
 
-# To install automatically on Ubuntu / RaspberryPi OS, copy => paste => run the command below in a
+# To install automatically on Ubuntu / RaspberryPi OS / Armbian, copy => paste => run the command below in a
 # terminal program (using the 'Terminal' app in the system menu, or over remote SSH), while logged in AS THE
 # USER THAT WILL RUN THE APP (user must have sudo privileges):
 
